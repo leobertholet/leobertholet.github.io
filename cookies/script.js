612fb0cc963246ae91224f11ffe950e33da4791e
leobertholet <55101284+leobertholet@users.noreply.github.com>
2021-06-11 16:08:37 -0400

    cookies basic

--- cookies/script.js
+++ cookies/script.js
@@ -15,25 +15,6 @@
 
 colWidth = 280;
 
-tilesInfo = [[50,
-              "Name",
-              "Here is some content for you sir"],
-             ["Name",
-              "Here is some content for you sir"],
-             ["Name",
-              "Here is some content for you sir"],
-             ["Name something words why how oh no",
-              "Here is some content for you sir"],
-             ["Name",
-              "Here is some content for you sir"],
-             ["Name",
-              "Here is some content for you sir and here how is that even"],
-             ["Name",
-              "Here is some content for you sir"],
-             ["Name",
-              "Here is some content for you sir"]
-            ];
-
 function getMinCol() {
     minCol = 0;
     var i;
@@ -47,28 +28,49 @@ function getMinCol() {
     return minCol;
 }
 
-function setCookie() {
-    if (!navigator.cookiesEnabled) {
-        alert("cookies not enabled!");
+function setCookie(index, string) {
+    localStorage.setItem(index.toString(), string);
+}
+
+function getCookie(index) {
+    return localStorage.getItem(index.toString());
+}
+
+function removeCookie(index) {
+    localStorage.removeItem(index.toString());
+}
+
+function storeCookies() {
+    localStorage.clear();
+    setCookie(-1,tilesInfo.length.toString());
+    var i;
+    for (i = 0; i < tilesInfo.length; i++) {
+        setCookie(2*i, tilesInfo[i][0]);
+        setCookie(2*i+1, tilesInfo[i][1]);
     }
-    else {
-        document.cookie = "username=John Doe";
+    if ($("#brightness-mode").text() == "Light mode") {
+        setCookie(-2, "dark");
     }
 }
 
-$("#cookies-test").click(function() {
-    setCookie();
-    console.log("done");
-});
+function loadCookies() {
+    len = getCookie(-1);
+    tilesInfo = [];
+    if (len >= 1) {
+        tilesInfo = new Array(len);
+    }
+
+    var i;
+    for (i = 0; i < len; i++) {
+        
+        tilesInfo[i] = [getCookie(2*i), getCookie(2*i+1)];
+    }
+}
 
 function pageSetup() {
     $("#container").empty();
     numCols = Math.max(1, Math.floor(0.85 * $(window).width() / (colWidth + 15)));
     colsWidth = Math.floor(numCols * colWidth + 15 * (numCols - 1));
-
-    windowWidth = $(window).innerWidth();
-
-    marginLeft = Math.floor((windowWidth - colsWidth) / 2);
 
     colHeights = new Array(numCols).fill(0);
 
@@ -84,6 +86,7 @@ function pageSetup() {
         tileInfo = tilesInfo[i];
         newTile = "<div class='tile'>"
                     + "<div class='delete'></div>"
+                    + "<div class ='move'></div>"
                     + "<h3>" + tileInfo[0] + "</h3>"
                     + "<p>" + tileInfo[1] + "</p>"
                 + "</div>";
@@ -101,6 +104,10 @@ function pageSetup() {
 
         colHeights[colNum] += newTile.height() + 10;
     }
+
+    if (tilesInfo.length == 0) {
+        $("#container").append("<p>You don't have any tiles yet ... click add :)</p>");
+    }
 }
 
 $(window).resize(function() {
@@ -108,46 +115,94 @@ $(window).resize(function() {
 });
 
 $(document).ready(function() {
+    loadCookies();
     pageSetup();
+    if (getCookie(-2) != null) {
+        darkMode();
+    }
+    /* move click() stuff into here */
+});
 
-    $(document).on("click", ".delete", function() {
-        if (confirm("Yo you sure?")) {
-            tile = $(this).parent();
-            index = tile.attr("index");
-            tile.remove();
-            tilesInfo.splice(index, 1); // Remove elt from arr
-            // reset indices of everyone that came after
-            $(".tile").each(function() {
-                if ($(this).attr("index") > index) {
-                    $(this).attr("index", $(this).attr("index") - 1);
-                }
-            });
-            pageSetup();
-        }
-    });
+/* swap tiles at i and j */
+function swapTiles(i, j) {
+    temp = tilesInfo[i];
+    tilesInfo[i] = tilesInfo[j];
+    tilesInfo[j] = temp;
+    storeCookies();
+    pageSetup();
+}
+
+$(document).on("click", ".delete", function() {
+    if (confirm("Yo you sure?")) {
+        tile = $(this).parent();
+        index = tile.attr("index");
+        tile.remove();
+        tilesInfo.splice(index, 1); // Remove elt from arr
+        // reset indices of everyone that came after
+        $(".tile").each(function() {
+            if ($(this).attr("index") > index) {
+                $(this).attr("index", $(this).attr("index") - 1);
+            }
+        });
+
+        pageSetup();
+        storeCookies();
+    }
+});
+
+function darkMode() {
+    $("#brightness-mode").text("Light mode");
+    $("body").css("background", "slategray");
+    setCookie(-2, "dark");
+}
+
+function lightMode() {
+    $("#brightness-mode").text("Dark mode");
+    $("body").css("background", "white");
+    removeCookie(-2);
+}
+
+$("#brightness-mode").click(function() {
+    if ($(this).text() == "Dark mode") {
+        darkMode();
+    }
+    else {
+        lightMode();
+    }
 });
 
 $("#add").click(function() {
     newTitle = prompt("enter title");
-    newText = prompt("enter text");
-    tilesInfo.push([newTitle, newText]);
+    if (newTitle != null) {
+        newText = prompt("enter text");
+    }
+    
+    if (newTitle != null && newText != null) {
+        tilesInfo.push([newTitle, newText]);
 
-    pageSetup();
+        pageSetup();
+        storeCookies();
+    }
 });
 
 $("#delete-all").click(function() {
     if (confirm("Umm for real?")) {
         tilesInfo = [];
         pageSetup();
+        storeCookies();
     }
 });
 
 $(document).on("click", ".tile h3", function() {
     newText = prompt("change text", $(this).text());
     $(this).text(newText);
+    tilesInfo[$(this).parent().attr("index")][0] = $(this).text();
+    storeCookies();
 });
 
 $(document).on("click", ".tile p", function() {
     newText = prompt("change text", $(this).text());
     $(this).text(newText);
+    tilesInfo[$(this).parent().attr("index")][1] = $(this).text();
+    storeCookies();
 });
